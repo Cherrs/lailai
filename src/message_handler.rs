529cc9af01393c335fff916140b5a7d5361e1ba7
KuriyamaMirai <lovef@outlook.com>
2023-02-19 04:24:13 +0800

    添加todo 可优化

--- src/message_handler.rs
+++ src/message_handler.rs
@@ -28,6 +28,7 @@ impl Handler for MyHandler {
                     "MESSAGE (GROUP={}): {}",
                     m.inner.group_code, m.inner.elements
                 );
+                // TODO:收到消息时需要clone一份消息，方便后面reply。是否可以不clone？
                 let reply = Reply {
                     reply_seq: m.inner.seqs[0],
                     sender: m.inner.from_uin,
@@ -298,7 +299,10 @@ async fn send_item_price_to_group(
         return msg;
     }
     let mut item_price_list = item_price.price_list.iter().take(8);
-    if let Ok(g) = client.upload_group_image(group_code, &item_price.icon).await {
+    if let Ok(g) = client
+        .upload_group_image(group_code, &item_price.icon)
+        .await
+    {
         msg.push(g);
     }
     msg.push(Text::new(item_price.name));
